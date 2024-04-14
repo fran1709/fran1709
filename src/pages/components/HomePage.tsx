1d60a37ee602c9f1424e4005f889b36ab7d1a70e
Francisco Ovares Rojas <82686857+fran1709@users.noreply.github.com>
2024-04-14 14:32:29 -0600

    nimodus

--- src/pages/components/HomePage.tsx
+++ src/pages/components/HomePage.tsx
@@ -3,8 +3,6 @@ import React from 'react';
 import About from '../sections/About';
 
 const Home: React.FC = () => {
-  console.log("hola home")
-  console.log(process.env.REACT_APP_FIREBASE_API_KEY)
   return (
     <div>
       <About/>
